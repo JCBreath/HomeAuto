a94972f3c349572d333290f52dc93eadcc535124
JCBreath <Mimafama0@qq.com>
2018-08-08 11:22:59 -0700

    8/9

--- SmartHomeWebMain/js/scene.js
+++ SmartHomeWebMain/js/scene.js
@@ -1,7 +1,18 @@
 var selected = 1;
 
-function init() {
 
+
+function init() {
+	console.log("initialize");
+
+	var oReq = new XMLHttpRequest();
+	var URL = "query?var=scene";
+
+	oReq.open("GET", URL);
+	oReq.addEventListener("load", function() {
+		select(this.responseText);
+	});
+	oReq.send();
 }
 
 function select(index) {
@@ -10,7 +21,17 @@ function select(index) {
 	content.style.height = "98%";
 	content.style.border = "2px solid #ebeffe";
 	content.style.background = "#5b5f9e";
-	selected = index;
+
+	console.log(index);
+
+	var oReq = new XMLHttpRequest();
+	var URL = "query?scene=" + index;
+
+	oReq.open("GET", URL);
+	oReq.addEventListener("load", function() {
+		selected = index;
+	});
+	oReq.send();
 }
 
 function reset_blocks() {
@@ -45,3 +66,5 @@ function mouseup(index) {
 	if(index != selected)
 		content.style.background = "#57968e";
 }
+
+init();
